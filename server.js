8d1ce7c652a2adb171cacba8b9fc49cf3d1fce36
Ran Zhuo <ryannezhuo@gmail.com>
2022-08-26 14:22:45 -0400

    fix server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const logger = require('morgan');
 
 
+
 const app = express();
 
 require('dotenv').config()
@@ -11,9 +12,12 @@ require('./config/database.js')
 app.use(logger('dev'));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/users', require('./routes/api/users'));
+app.use(require('./config/auth'));
 
+//app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
