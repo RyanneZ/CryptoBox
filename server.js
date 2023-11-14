4929e4d1109c99a2a2acf18ba57352b3744b297c
Ryanne <91228440+RyanneZ@users.noreply.github.com>
2023-11-14 09:52:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
